54659e809111af0c207f8775c529359752a73b31
vaibhav <vaibhav.shaharwale@outlook.com>
2023-02-18 20:46:26 +0530

    done/tested

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
+import numpy as np
+from tensorflow import keras
+model = keras.models.load_model("keypoint_classifier.h5")
 import cv2
 import mediapipe as mp
 import numpy as np
-
-#import csv
-#import argparse
-#import itertools
-#from  collections import Counter
-#from collections import deque
+import pickle
+dict = pickle.load(open('labels.pickel', 'rb'))
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -16,6 +15,7 @@ mp_hands = mp.solutions.hands
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
     model_complexity=0,
+    max_num_hands=1,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
   while cap.isOpened():
@@ -35,13 +35,17 @@ with mp_hands.Hands(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    prediction = []
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
+        fin_co = []
         for i, lm in enumerate(hand_landmarks.landmark):
-            x = int(lm.x*width)
-            y = int(lm.y*height)
-            arr = np.array([x, y])
-            print(i, arr)
+            #x = int(lm.x*width)
+            #y = int(lm.y*height)
+            fin_co.append(lm.x)
+            fin_co.append(lm.y)
+            #arr = np.array([x, y])
+            #print(i, arr)
             #if i == 8:
              #   cv2.circle(image, (x, y), 10,  (0, 0, 255), cv2.FILLED)
         
@@ -52,8 +56,12 @@ with mp_hands.Hands(
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
         # Flip the image horizontally for a selfie-view display.
+        pred = model.predict(np.array([fin_co]), verbose=0)
+        pred = np.argmax(pred)
+        prediction.append(dict[pred])
     #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)    # for grayscale
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    image = cv2.putText(cv2.flip(image, 1), str(prediction),(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.imshow('Sign language', image)
     if cv2.waitKey(5) & 0xFF == ord('q'):
      break
 cap.release()
